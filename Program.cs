f248103b2948a7d05935ec651c3db0291e665d2f
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-05-22 19:42:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quilômetros e centímetros tem dentro de 150 metros?");
+            double M=150;
+            double resultado1=M * 100;
+            double resultado2=M / 1000;
+            Console.Write("Centímetros=");
+            Console.WriteLine($"{resultado1}");
+            Console.WriteLine("Metros=150");
+            Console.Write("Quilometros=");
+            Console.WriteLine($"{resultado2}");
+            
+        }
+    }
+}
