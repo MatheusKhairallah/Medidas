12dd0b37b20974d4f04ab2e5a71ec9645d956ae9
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:20:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva um número que representará o valor de uma medida em metros:");
+            double M=Convert.ToDouble(Console.ReadLine());
+            double resultado1=M * 100;
+            double resultado2=M / 1000;
+            Console.Clear();
+            Console.WriteLine($"Centímetros={resultado1}");
+            Console.WriteLine("Metros=" +M);
+            Console.WriteLine($"Quilometros={resultado2}");
+            
+        }
+    }
+}
